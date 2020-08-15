c9bd13fbede61b2dbf584a12f886941c2a0b32d1
Abolfazl26s <asaeidabadi243@gmail.com>
2020-08-15 13:31:14 +0430

    Updates

---
+++ precache-manifest.5bbbb897da9e0aafafb5934a5c6e0398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef82d3917b0bb520452319a10224157",
+    "url": "/zeus-project/index.html"
+  },
+  {
+    "revision": "38c7b7fb6ca8a8b167a1",
+    "url": "/zeus-project/static/css/main.c5d97991.chunk.css"
+  },
+  {
+    "revision": "fcf0b9a5935b7c147b7e",
+    "url": "/zeus-project/static/js/2.169c729b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zeus-project/static/js/2.169c729b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c7b7fb6ca8a8b167a1",
+    "url": "/zeus-project/static/js/main.b2fae786.chunk.js"
+  },
+  {
+    "revision": "3b1313f1208e9c4593f4",
+    "url": "/zeus-project/static/js/runtime-main.1e55ec05.js"
+  }
+]);
